11d94949c0bc666a2286d49795cce71ea04ca86d
Claire_M <clairerodriguez79@yahoo.fr>
2023-05-14 12:33:29 +0200

    installation et test scss ok

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import styles from './page.module.css'
+// import styles from './page.module.css'
+import styles from '../styles/home.module.scss'
 
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className={styles.highlightscss}>
       <h1>Home Page</h1>
       <p>
         <Link href="/users">Users</Link>
--- app/users/page.js
+++ app/users/page.js
@@ -1,5 +1,6 @@
 import getAllUsers from '/lib/getAllUsers'
 import Link from "next/link"
+import styles from '../../styles/user.module.scss'
 
 
 export default async function UsersPage() {
@@ -10,7 +11,8 @@ export default async function UsersPage() {
     // console.log("log users",users) 
 
     const content = (
-        <section>
+        <section className={styles.highlightscss}>
+            <h1>Users Page</h1>
             <h2>
                 <Link href="/">Back to Home</Link>
             </h2>
